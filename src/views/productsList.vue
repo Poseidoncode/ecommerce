efb9d24cd06973e551e0fb3baa9e2e83bb9a556b
Poseidoncode <posidomhu@gmail.com>
2022-05-24 13:28:58 +0800

    api

--- src/views/productsList.vue
+++ src/views/productsList.vue
@@ -29,21 +29,6 @@
           >
             <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3" />
           </svg>
-          <!-- <svg
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="sliders-h"
-            class="svg-inline--fa fa-sliders-h fa-w-16 text-xl ml-2"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-          >
-            <path
-              fill="currentColor"
-              d="M496 384H160v-16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h80v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h336c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm0-160h-80v-16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h336v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h80c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm0-160H288V48c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v16H16C7.2 64 0 71.2 0 80v32c0 8.8 7.2 16 16 16h208v16c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16v-16h208c8.8 0 16-7.2 16-16V80c0-8.8-7.2-16-16-16z"
-            ></path>
-          </svg> -->
         </div>
 
         <div
@@ -250,273 +235,12 @@
           <div
             class="grid grid-cols-1 sm:grid-cols-3 xl:grid-cols-4 gap-6 mt-12"
           >
-            <a href="#"
+            <a href="#" v-for="(item, idx) in items" :key="`content${idx}`"
               ><div
                 class="mx-auto cursor-pointer h-full hover:border-gray-400 transform transition-all duration-200 ease hover:-translate-y-1 shadow-sm w-72 max-w-full border border-gray-300 rounded-sm bg-white"
               >
                 <div class="w-full h-48">
-                  <img
-                    src="https://images.unsplash.com/photo-1566740933430-b5e70b06d2d5?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60"
-                    class="w-full h-full object-cover"
-                  />
-                </div>
-                <div class="p-6">
-                  <div class="text-sm">
-                    <h3 class="font-bold text-base">Burritown</h3>
-                    <div class="flex items-center text-green-400">
-                      <svg
-                        class="h-5 w-5 text-yellow-400"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <polygon
-                          points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        />
-                      </svg>
-                      4.7
-                    </div>
-                    <p class="mt-1">
-                      <span>Burritos · </span><span>Salads · </span
-                      ><span>Mexican · </span><span>Chicken </span>
-                    </p>
-                    <p>1.5 miles away · $$</p>
-                  </div>
-                </div>
-              </div></a
-            ><a href="#"
-              ><div
-                class="mx-auto cursor-pointer h-full hover:border-gray-400 transform transition-all duration-200 ease hover:-translate-y-1 shadow-sm w-72 max-w-full border border-gray-300 rounded-sm bg-white"
-              >
-                <div class="w-full h-48">
-                  <img
-                    src="https://images.unsplash.com/photo-1579871494447-9811cf80d66c?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60"
-                    class="w-full h-full object-cover"
-                  />
-                </div>
-                <div class="p-6">
-                  <div class="text-sm">
-                    <h3 class="font-bold text-base">Sushi Blue</h3>
-                    <div class="flex items-center text-green-400">
-                      <svg
-                        class="h-5 w-5 text-yellow-400"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <polygon
-                          points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        />
-                      </svg>
-                      4.2
-                    </div>
-                    <p class="mt-1">
-                      <span>Sushi · </span><span>Japanese </span>
-                    </p>
-                    <p>1.1 miles away · $$</p>
-                  </div>
-                </div>
-              </div></a
-            ><a href="#"
-              ><div
-                class="mx-auto cursor-pointer h-full hover:border-gray-400 transform transition-all duration-200 ease hover:-translate-y-1 shadow-sm w-72 max-w-full border border-gray-300 rounded-sm bg-white"
-              >
-                <div class="w-full h-48">
-                  <img
-                    src="https://images.unsplash.com/photo-1577859623802-b5e3ca51f885?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60"
-                    class="w-full h-full object-cover"
-                  />
-                </div>
-                <div class="p-6">
-                  <div class="text-sm">
-                    <h3 class="font-bold text-base">Three Aunts</h3>
-                    <div class="flex items-center text-green-400">
-                      <svg
-                        class="h-5 w-5 text-yellow-400"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <polygon
-                          points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        />
-                      </svg>
-                      4.9
-                    </div>
-                    <p class="mt-1">
-                      <span>Chinese · </span><span>BBQ · </span
-                      ><span>Chicken · </span><span>Noodles </span>
-                    </p>
-                    <p>2.5 miles away · $$$</p>
-                  </div>
-                </div>
-              </div></a
-            ><a href="#"
-              ><div
-                class="mx-auto cursor-pointer h-full hover:border-gray-400 transform transition-all duration-200 ease hover:-translate-y-1 shadow-sm w-72 max-w-full border border-gray-300 rounded-sm bg-white"
-              >
-                <div class="w-full h-48">
-                  <img
-                    src="https://images.unsplash.com/photo-1542676303584-c8043a6c7618?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60"
-                    class="w-full h-full object-cover"
-                  />
-                </div>
-                <div class="p-6">
-                  <div class="text-sm">
-                    <h3 class="font-bold text-base">Farmer P</h3>
-                    <div class="flex items-center text-green-400">
-                      <svg
-                        class="h-5 w-5 text-yellow-400"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <polygon
-                          points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        />
-                      </svg>
-                      4.2
-                    </div>
-                    <p class="mt-1">
-                      <span>Chicken · </span><span>Mediterranean · </span
-                      ><span>British · </span><span>Healthy </span>
-                    </p>
-                    <p>3.5 miles away · $</p>
-                  </div>
-                </div>
-              </div></a
-            ><a href="#"
-              ><div
-                class="mx-auto cursor-pointer h-full hover:border-gray-400 transform transition-all duration-200 ease hover:-translate-y-1 shadow-sm w-72 max-w-full border border-gray-300 rounded-sm bg-white"
-              >
-                <div class="w-full h-48">
-                  <img
-                    src="https://images.unsplash.com/photo-1572137162111-fc6e04414e21?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60"
-                    class="w-full h-full object-cover"
-                  />
-                </div>
-                <div class="p-6">
-                  <div class="text-sm">
-                    <h3 class="font-bold text-base">Bagel King</h3>
-                    <div class="flex items-center text-green-400">
-                      <svg
-                        class="h-5 w-5 text-yellow-400"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <polygon
-                          points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        />
-                      </svg>
-                      4.1
-                    </div>
-                    <p class="mt-1">
-                      <span>Bagels · </span><span>Breakfast · </span
-                      ><span>Dessert </span>
-                    </p>
-                    <p>1.6 miles away · $</p>
-                  </div>
-                </div>
-              </div></a
-            ><a href="#"
-              ><div
-                class="mx-auto cursor-pointer h-full hover:border-gray-400 transform transition-all duration-200 ease hover:-translate-y-1 shadow-sm w-72 max-w-full border border-gray-300 rounded-sm bg-white"
-              >
-                <div class="w-full h-48">
-                  <img
-                    src="https://images.unsplash.com/photo-1571917411767-20545014a0bc?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60"
-                    class="w-full h-full object-cover"
-                  />
-                </div>
-                <div class="p-6">
-                  <div class="text-sm">
-                    <h3 class="font-bold text-base">Mr. Banh Mi</h3>
-                    <div class="flex items-center text-green-400">
-                      <svg
-                        class="h-5 w-5 text-yellow-400"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <polygon
-                          points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        />
-                      </svg>
-                      4.6
-                    </div>
-                    <p class="mt-1">
-                      <span>Sandwiches · </span><span>Vietnamese · </span
-                      ><span>Noodles · </span><span>Drinks </span>
-                    </p>
-                    <p>1.5 miles away · $$</p>
-                  </div>
-                </div>
-              </div></a
-            ><a href="#"
-              ><div
-                class="mx-auto cursor-pointer h-full hover:border-gray-400 transform transition-all duration-200 ease hover:-translate-y-1 shadow-sm w-72 max-w-full border border-gray-300 rounded-sm bg-white"
-              >
-                <div class="w-full h-48">
-                  <img
-                    src="https://images.unsplash.com/photo-1557872943-16a5ac26437e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60"
-                    class="w-full h-full object-cover"
-                  />
-                </div>
-                <div class="p-6">
-                  <div class="text-sm">
-                    <h3 class="font-bold text-base">Fancy Ramen</h3>
-                    <div class="flex items-center text-green-400">
-                      <svg
-                        class="h-5 w-5 text-yellow-400"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <polygon
-                          points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"
-                        />
-                      </svg>
-                      4.8
-                    </div>
-                    <p class="mt-1">
-                      <span>Ramen · </span><span>Noodles · </span
-                      ><span>Japanese </span>
-                    </p>
-                    <p>3.2 miles away · $$$$</p>
-                  </div>
-                </div>
-              </div></a
-            ><a href="#"
-              ><div
-                class="mx-auto cursor-pointer h-full hover:border-gray-400 transform transition-all duration-200 ease hover:-translate-y-1 shadow-sm w-72 max-w-full border border-gray-300 rounded-sm bg-white"
-              >
-                <div class="w-full h-48">
-                  <img
-                    src="https://images.unsplash.com/photo-1579751626657-72bc17010498?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60"
-                    class="w-full h-full object-cover"
-                  />
+                  <img :src="`${item.imageUrl}`" />
                 </div>
                 <div class="p-6">
                   <div class="text-sm">
@@ -554,13 +278,39 @@
 </template>
 
 <script>
-import { inject, ref, defineComponent } from "vue";
+import { defineComponent, ref, onMounted, watch, inject } from 'vue';
+import { getCustomerProductAll } from 'Service/apis.js';
+import { useToast } from 'vue-toastification';
 
 export default defineComponent({
   props: {},
   setup(props, { emit }) {
     // emit("update:modelValue", _newValues);
-    return {};
+    const toast = useToast();
+    const items = ref([]);
+    const getData = async () => {
+      try {
+        //top:筆數、skip:跳過幾筆
+        const page = 1;
+        // const page = +offset.value / +rows.value + +1;
+        // const skip = (page - 1) * rows.value;
+        // const top = rows.value;
+
+        const res = await getCustomerProductAll(`?page=${page}`);
+
+        items.value = [...res.data?.products];
+        // totalItemsCount.value = +res.data?.length;
+      } catch (e) {
+        toast.error(`${e.response ? e.response.data : e}`, {
+          timeout: 2000,
+          hideProgressBar: true,
+        });
+      }
+    };
+    onMounted(async () => {
+      await getData();
+    });
+    return { items, getData };
   },
 });
 </script>
