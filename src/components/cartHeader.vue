5257f91c71e23ac8753b754b303c10d75d6c8fac
Poseidoncode <66759971+Poseidoncode@users.noreply.github.com>
2022-05-30 00:51:40 +0800

    product

--- src/components/cartHeader.vue
+++ src/components/cartHeader.vue
@@ -1,8 +1,5 @@
 <template>
-  <header
-    class="header-content"
-    :class="!scrollIsZero || $route.name != 'home' ? 'addBg' : ''"
-  >
+  <header class="header-content" :class="!scrollIsZero || $route.name != 'home' ? 'addBg' : ''">
     <div class="container mx-auto px-6 py-3">
       <div class="flex items-center justify-between">
         <div class="hidden w-full text-gray-600 md:flex md:items-center">
@@ -27,137 +24,69 @@
             </svg> -->
           <!-- <span class="mx-1 text-sm">NY</span> -->
         </div>
-        <div
-          class="w-full text-gray-700 md:text-center text-2xl font-semibold flex justify-center"
-        >
+        <div class="w-full text-gray-700 md:text-center text-2xl font-semibold flex justify-center">
           <!-- Brand -->
           <img class="mr-2" style="height: 78px" src="/assets/brandfinal.png" />
         </div>
 
         <div class="flex items-center justify-end w-full">
-          <button
-            @click="$router.push('/login')"
-            style="margin-right: 16px"
-            class="text-gray-400 focus:outline-none mx-8 sm:mx-0 mr-10"
-            :class="!scrollIsZero ? 'text-gray-600' : ''"
-          >
-            <svg
-              class="h-6 w-6"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
+          <button @click="$router.push('/login')" style="margin-right: 16px"
+            class="text-gray-400 focus:outline-none mx-8 sm:mx-0 mr-10" :class="!scrollIsZero ? 'text-gray-600' : ''">
+            <svg class="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+              stroke-linecap="round" stroke-linejoin="round">
               <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
               <circle cx="12" cy="7" r="4" />
             </svg>
           </button>
-          <button
-            @click="$router.push('/login')"
-            style="margin-right: 16px"
-            class="text-gray-400 focus:outline-none mx-8 sm:mx-0"
-            :class="!scrollIsZero ? 'text-gray-600' : ''"
-          >
-            <svg
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-              />
+          <button @click="$router.push('/login')" style="margin-right: 16px"
+            class="text-gray-400 focus:outline-none mx-8 sm:mx-0" :class="!scrollIsZero ? 'text-gray-600' : ''">
+            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
             </svg>
           </button>
-          <button
-            @click="cartOpen = !cartOpen"
-            class="text-gray-400 focus:outline-none mx-4 sm:mx-0"
-            :class="!scrollIsZero ? 'text-gray-600' : ''"
-          >
-            <svg
-              class="h-6 w-6"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
+          <button @click="cartOpen = !cartOpen" class="text-gray-400 focus:outline-none mx-4 sm:mx-0"
+            :class="!scrollIsZero ? 'text-gray-600' : ''">
+            <svg class="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              viewBox="0 0 24 24" stroke="currentColor">
               <path
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              ></path>
+                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
+              </path>
             </svg>
           </button>
 
           <div class="flex sm:hidden">
-            <button
-              @click="isOpen = !isOpen"
-              type="button"
-              class="text-gray-600 hover:text-gray-500 focus:outline-none focus:text-gray-500"
-              aria-label="toggle menu"
-            >
+            <button @click="isOpen = !isOpen" type="button"
+              class="text-gray-600 hover:text-gray-500 focus:outline-none focus:text-gray-500" aria-label="toggle menu">
               <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
-                <path
-                  fill-rule="evenodd"
-                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                ></path>
+                <path fill-rule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
+                </path>
               </svg>
             </button>
           </div>
         </div>
       </div>
-      <nav
-        :class="isOpen ? '' : 'hidden'"
-        class="sm:flex sm:justify-center sm:items-center mt-4"
-      >
+      <nav :class="isOpen ? '' : 'hidden'" class="sm:flex sm:justify-center sm:items-center mt-4">
         <div class="flex flex-col sm:flex-row">
-          <a class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0" href="#"
-            >Home</a
-          >
-          <a
-            class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0"
-            href="#"
-            @click.prevent="
-              isOpen = false;
-              $router.push('/productslist');
-            "
-            >Shop</a
-          >
-          <a class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0" href="#"
-            >Categories</a
-          >
-          <a
-            class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0"
-            @click.prevent="
-              isOpen = false;
-              $router.push('/faq');
-            "
-            href="#"
-            >FAQ</a
-          >
-          <a
-            class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0"
-            href="#"
-            @click.prevent="
-              isOpen = false;
-              $router.push('/news');
-            "
-            >News</a
-          >
-          <a
-            class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0"
-            href="#"
-            @click.prevent="
-              isOpen = false;
-              $router.push('/about');
-            "
-            >About</a
-          >
+          <a class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0" href="#">Home</a>
+          <a class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0" href="#" @click.prevent="
+            isOpen = false;
+          $router.push('/productslist');
+          ">Shop</a>
+          <a class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0" href="#">Contact</a>
+          <a class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0" @click.prevent="
+            isOpen = false;
+          $router.push('/faq');
+          " href="#">FAQ</a>
+          <a class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0" href="#" @click.prevent="
+            isOpen = false;
+          $router.push('/news');
+          ">News</a>
+          <a class="mt-3 text-gray-400 hover:underline sm:mx-3 sm:mt-0" href="#" @click.prevent="
+            isOpen = false;
+          $router.push('/about');
+          ">About</a>
         </div>
       </nav>
       <!-- <div class="relative mt-6 max-w-lg mx-auto">
@@ -181,26 +110,14 @@
         </div> -->
     </div>
   </header>
-  <div
-    :class="cartOpen ? 'translate-x-0 ease-out' : 'translate-x-full ease-in'"
+  <div :class="cartOpen ? 'translate-x-0 ease-out' : 'translate-x-full ease-in'"
     class="fixed right-0 top-0 max-w-xs w-full h-full px-6 py-4 transition duration-300 transform overflow-y-auto bg-white border-l-2 border-gray-300"
-    style="z-index: 100"
-  >
+    style="z-index: 100">
     <div class="flex items-center justify-between">
       <h3 class="text-2xl font-medium text-gray-700">Your cart</h3>
-      <button
-        @click="cartOpen = !cartOpen"
-        class="text-gray-600 focus:outline-none"
-      >
-        <svg
-          class="h-5 w-5"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
+      <button @click="cartOpen = !cartOpen" class="text-gray-600 focus:outline-none">
+        <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          viewBox="0 0 24 24" stroke="currentColor">
           <path d="M6 18L18 6M6 6l12 12"></path>
         </svg>
       </button>
@@ -208,44 +125,22 @@
     <hr class="my-3" />
     <div class="flex justify-between mt-6">
       <div class="flex">
-        <img
-          class="h-20 w-20 object-cover rounded"
+        <img class="h-20 w-20 object-cover rounded"
           src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
-          alt=""
-        />
+          alt="" />
         <div class="mx-3">
           <h3 class="text-sm text-gray-600">Mac Book Pro</h3>
           <div class="flex items-center mt-2">
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
+            <button class="text-gray-500 focus:outline-none focus:text-gray-600">
+              <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                viewBox="0 0 24 24" stroke="currentColor">
+                <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </button>
             <span class="text-gray-700 mx-2">2</span>
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
+            <button class="text-gray-500 focus:outline-none focus:text-gray-600">
+              <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                viewBox="0 0 24 24" stroke="currentColor">
                 <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </button>
@@ -256,44 +151,22 @@
     </div>
     <div class="flex justify-between mt-6">
       <div class="flex">
-        <img
-          class="h-20 w-20 object-cover rounded"
+        <img class="h-20 w-20 object-cover rounded"
           src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
-          alt=""
-        />
+          alt="" />
         <div class="mx-3">
           <h3 class="text-sm text-gray-600">Mac Book Pro</h3>
           <div class="flex items-center mt-2">
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
+            <button class="text-gray-500 focus:outline-none focus:text-gray-600">
+              <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                viewBox="0 0 24 24" stroke="currentColor">
+                <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </button>
             <span class="text-gray-700 mx-2">2</span>
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
+            <button class="text-gray-500 focus:outline-none focus:text-gray-600">
+              <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                viewBox="0 0 24 24" stroke="currentColor">
                 <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </button>
@@ -304,44 +177,22 @@
     </div>
     <div class="flex justify-between mt-6">
       <div class="flex">
-        <img
-          class="h-20 w-20 object-cover rounded"
+        <img class="h-20 w-20 object-cover rounded"
           src="https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"
-          alt=""
-        />
+          alt="" />
         <div class="mx-3">
           <h3 class="text-sm text-gray-600">Mac Book Pro</h3>
           <div class="flex items-center mt-2">
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
+            <button class="text-gray-500 focus:outline-none focus:text-gray-600">
+              <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                viewBox="0 0 24 24" stroke="currentColor">
+                <path d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </button>
             <span class="text-gray-700 mx-2">2</span>
-            <button
-              class="text-gray-500 focus:outline-none focus:text-gray-600"
-            >
-              <svg
-                class="h-5 w-5"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
+            <button class="text-gray-500 focus:outline-none focus:text-gray-600">
+              <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                viewBox="0 0 24 24" stroke="currentColor">
                 <path d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </button>
@@ -352,31 +203,18 @@
     </div>
     <div class="mt-8">
       <form class="flex items-center justify-center">
-        <input
-          class="form-input w-48"
-          type="text"
-          placeholder="Add promocode"
-        />
+        <input class="form-input w-48" type="text" placeholder="Add promocode" />
         <button
-          class="ml-3 flex items-center px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
-        >
+          class="ml-3 flex items-center px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
           <span>Apply</span>
         </button>
       </form>
     </div>
     <a
-      class="flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
-    >
+      class="flex items-center justify-center mt-4 px-3 py-2 bg-blue-600 text-white text-sm uppercase font-medium rounded hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
       <span>Chechout</span>
-      <svg
-        class="h-5 w-5 mx-2"
-        fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
+      <svg class="h-5 w-5 mx-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+        viewBox="0 0 24 24" stroke="currentColor">
         <path d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
       </svg>
     </a>
@@ -386,7 +224,7 @@
 import { inject, ref, defineComponent } from "vue";
 
 export default defineComponent({
-  setup() {
+  setup () {
     const isOpen = ref(false);
     const cartOpen = ref(false);
     const scrollIsZero = ref(false);
@@ -410,7 +248,8 @@ export default defineComponent({
   },
 });
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+</style>
 <style lang="scss">
 .header-content {
   position: fixed;
@@ -426,18 +265,19 @@ export default defineComponent({
   background-color: rgb(255 255 255 / 89%);
   -webkit-backdrop-filter: saturate(180%) blur(20px);
   backdrop-filter: saturate(180%) blur(20px);
-  box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px rgba(0, 0, 0, 0.05) 0px
-    1px 1px 0px;
+  box-shadow: rgba(50, 50, 105, 0.15) 0px 2px 5px 0px rgba(0, 0, 0, 0.05) 0px 1px 1px 0px;
   // border-bottom: 1px solid #2d3858;
   box-shadow: 0 0 10px rgb(4 7 14 / 80%);
   z-index: 99;
-  > div.container {
-    > nav {
+
+  >div.container {
+    >nav {
       a {
         color: #4b5562 !important;
       }
     }
   }
+
   /* // header.addBg > div > nav > div > a:nth-child(5) */
 }
 </style>
